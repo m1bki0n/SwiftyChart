af1512bbcb3d90b003b32e5dadccb87b0e86796d
Makwan BK <contact@makwanbk.com>
2021-01-26 15:04:21 +0300

    Initial Commit

---
+++ Shared/SwiftyChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyChartApp.swift
+//  Shared
+//
+//  Created by Makwan BK on 2021-01-26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
